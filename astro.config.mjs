afa5c9e7f8e327c1cea454f9aeb8dc9d6549d521
Nicolas Arbelaez <111522939+nicoarbelaez@users.noreply.github.com>
2025-03-25 10:35:42 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nicoarbelaez.github.io",
-  base: "landing-orderlys",
+  base: "landing-orderlys-temp",
   integrations: [
     react(),
     tailwind({
